75b99aed58e83e9d0531f278e8b30ef59f64b1e0
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-12 18:19:02 +0100

    Fixed no-existing post thumbnail error

--- wp-content/themes/followAndrew/template-parts/content-archive.php
+++ wp-content/themes/followAndrew/template-parts/content-archive.php
@@ -1,7 +1,9 @@
 <div class="container">
     <div class="post mb-5">
         <div class="media">
-            <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php get_the_post_thumbnail_url(); ?>" alt="image">
+            <?php if(has_post_thumbnail()) { ?>
+            <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php the_post_thumbnail_url(); ?>" alt="image">
+            <?php } ?>
             <div class="media-body">
                 <h3 class="title mb-1"><?php the_title(); ?></h3>
                 <div class="meta mb-1">
