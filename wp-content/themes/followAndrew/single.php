becb019342c01e71350b34994560d1b13c9e7271
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-12 16:32:11 +0100

    Created single.php

--- wp-content/themes/followAndrew/single.php
+++ wp-content/themes/followAndrew/single.php
@@ -1,3 +1,23 @@
-<?php
+<?php get_header(); ?>
 
-// Displays single post
+
+    <!-- Front page file loads the content directly to the Front page -> No matter what -->
+
+    <article class="content px-3 py-5 p-md-5">
+
+        <?php
+
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post(); // This function is kinda handler that will help me get rest of data about posts
+
+                the_content();
+            }
+        }
+
+        ?>
+
+    </article>
+
+
+<?php get_footer(); ?>
