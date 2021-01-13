325841300222800d128eced40f8086f2e875f95a
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-13 15:37:42 +0100

    Added menus widget and set up menus location

--- wp-content/themes/MisFlow/functions.php
+++ wp-content/themes/MisFlow/functions.php
@@ -1,8 +1,26 @@
 <?php
 
+
+
+
 function misflow_add_theme_support(): void {
 	add_theme_support('title-tag');
 	add_theme_support('custom-logo');
+	add_theme_support( 'menus' );
 }
 
 add_action('after_setup_theme', 'misflow_add_theme_support');
+
+
+
+
+function misflow_menus_location(): void {
+	$locations = array(
+		'primary'   => 'Primary Navigation',
+		'footer'    => 'Footer Navigation'
+	);
+
+	register_nav_menus($locations);
+}
+
+add_action('init', 'missflow_menus');
