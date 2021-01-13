122bb4941f044656abda95bde07dc72725925c95
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-13 14:51:50 +0100

    Removed unnecessary theme supports for now

--- wp-content/themes/MisFlow/functions.php
+++ wp-content/themes/MisFlow/functions.php
@@ -2,9 +2,6 @@
 
 function misflow_add_theme_support(): void {
 	add_theme_support('title-tag');
-	add_theme_support('custom-logo');
-	add_theme_support('post-thumbnails');
-	add_theme_support('custom-header');
 }
 
 add_action('after_setup_theme', 'misflow_add_theme_support');
