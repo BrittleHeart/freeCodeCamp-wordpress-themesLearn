99471ac08d002194966210ed4c08e36622d801b7
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-13 14:38:45 +0100

    Added theme supports

--- wp-content/themes/MisFlow/functions.php
+++ wp-content/themes/MisFlow/functions.php
@@ -1,1 +1,10 @@
 <?php
+
+function misflow_add_theme_support(): void {
+	add_theme_support('title-tag');
+	add_theme_support('custom-logo');
+	add_theme_support('post-thumbnails');
+	add_theme_support('custom-header');
+}
+
+add_action('after_setup_theme', 'misflow_add_theme_support');
