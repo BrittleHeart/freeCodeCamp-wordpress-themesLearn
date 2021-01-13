653ebcc7a014ce2c4119ff2fc1af3a33a0379780
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-13 14:04:13 +0100

    Moved footer from front-page.php -> footer.php

--- wp-content/themes/MisFlow/footer.php
+++ wp-content/themes/MisFlow/footer.php
@@ -1,1 +1,20 @@
-<?php
+<footer class="missflow-theme__footer" role="group" aria-label="Footer"> <!-- .missflow-theme__footer -->
+
+	<section class="footer__copyright"> <!-- .missflow-theme__footer__copyright -->
+
+		<h4 class="footer__copyright-header"> <!-- .missflow-theme__footer__copyright-header -->
+			<?php echo get_bloginfo('author_email'); ?>
+			&copy;
+			<?php echo date('Y'); ?>
+		</h4> <!-- end .missflow-theme__footer__copyright-header -->
+
+	</section> <!-- end .missflow-theme__footer__copyright -->
+
+</footer> <!-- end .missflow-theme__footer -->
+
+
+<!-- JavaScript imports -->
+<script src="assets/main.js"></script>
+
+</body>
+</html>
