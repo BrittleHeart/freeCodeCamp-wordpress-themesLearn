9a6f03647a4920d40cb990dddc3c37b8865763b0
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-14 16:39:29 +0100

    Created html template for 404.php page

--- wp-content/themes/MisFlow/404.php
+++ wp-content/themes/MisFlow/404.php
@@ -1,1 +1,8 @@
-<?php
+<?php get_header(); ?>
+
+<section class="misflow-404">
+	<h1 class="misflow-404-header">Could not find target page</h1>
+    <p class="misflow-404-description">You can try to find the some other page</p>
+</section>
+
+<?php get_footer(); ?>
