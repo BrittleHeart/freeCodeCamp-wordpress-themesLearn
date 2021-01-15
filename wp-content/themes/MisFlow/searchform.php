f781d2f2453e51e2895c7a32dd3adbfac41407cf
Bartosz Pazdur <b.pazdur@onet.pl>
2021-01-15 12:54:53 +0100

    Modified searchform.php template

--- wp-content/themes/MisFlow/searchform.php
+++ wp-content/themes/MisFlow/searchform.php
@@ -1,1 +1,15 @@
-<form action="" method="post"></form>
+<form action="<?php esc_url(home_url()); ?>" method="post" class="misflow-theme__search-form">
+    <section class="misflow-theme__search-form__group" role="search">
+        <label for="search_input">Search for: </label>
+        <input
+                type="search"
+                role="textbox"
+                aria-label="Search site:"
+                id="search_input"
+                placeholder="<?php esc_html_e('Please type your looked for resource', 'misflow'); ?>">
+
+        <button type="button" class="misflow-theme__search-form-button" role="button">
+            <?php esc_html_e('Search resource', 'misflow'); ?>
+        </button>
+    </section>
+</form>
